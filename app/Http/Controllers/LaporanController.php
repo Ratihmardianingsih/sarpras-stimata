9a92a70894c1a410ee5db4a5760a994dd1b51817
Ratih <ratih_22310002@stimata.ac.id>
2025-05-15 18:39:18 +0700

    update Kembalikan button

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Laporan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LaporanController extends Controller
 {
     public function index()
     {
-    
-        $laporanData = Laporan::with(['user', 'ruangan'])->get();
+        // Ambil data laporan yang sesuai dengan user yang sedang login
+        $laporanData = Laporan::with(['user', 'ruangan'])
+                            ->where('user_id', Auth::id()) // Filter berdasarkan ID user yang login
+                            ->get();
+
         return view('laporan.index', compact('laporanData'));
     }
 }
